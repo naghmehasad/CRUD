e1c33cc7a9585596cff94c5a92d32b39adfe6567
Naghmeh <naghmeh.asad@gmail.com>
2022-04-15 13:00:28 +0430

    Upload simple CURD project on Github

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,24 @@
+@extends('layout')
+
+@section('title','contact')
+
+@section('content')
+        <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
+
+            <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
+                <div class="flex justify-center pt-8 sm:justify-start sm:pt-0">
+                   <h1>Contact Us</h1>
+                </div>
+
+                <div class="mt-8 dark:bg-gray-800 overflow-hidden ">
+                   <p>this is contact page </p>
+                </div>
+@endsection
+
+
+@section('scripts')
+
+<script>
+alert('contact us')
+</script>
+@endsection
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,14 @@
+@extends('layout')
+
+@section('title','home')
+
+@section('content')
+            <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
+                <div class="flex justify-center pt-8 sm:justify-start sm:pt-0">
+                   <h1>Home</h1> 
+                </div>
+
+                <div class="mt-8 dark:bg-gray-800 overflow-hidden ">
+                   <p>this is Home page </p>
+                </div>
+@endsection
